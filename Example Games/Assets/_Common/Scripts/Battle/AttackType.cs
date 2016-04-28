280cef1b04afeb9345c1940360540ec2c411e2b1
robbiegascoigne <robbie.gascoigne@jacks.sdstate.edu>
2016-04-28 15:16:32 -0500

    Removed defunct dialog project & example projects.
    No longer need these items.

--- Example Games/Assets/_Common/Scripts/Battle/AttackType.cs
+++
@@ -1,3 +0,0 @@
-public enum AttackType {
-	Damage, Heal, Sleep
-}
--- Example Games/Assets/_Common/Scripts/BillboardScript.cs
+++
@@ -1,8 +0,0 @@
-﻿using UnityEngine;
-
-public class BillboardScript : MonoBehaviour {
-
-	void Update() {
-		transform.LookAt(Camera.main.transform.position, -Vector3.up);
-	}
-}
