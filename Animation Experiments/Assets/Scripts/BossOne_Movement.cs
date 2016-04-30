35cc99449b4c5cc50f4f36b974246ff9c14f8876
Tecku <sethbreloom94@gmail.com>
2016-04-30 13:23:15 -0500

    Restoring Animation Experiments
    Here is the folder again.

---
+++ Animation Experiments/Assets/Scripts/BossOne_Movement.cs
@@ -0,0 +1,172 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BossOne_Movement : MonoBehaviour {
+
+	enum B1State { Idle, Align, Attack, Tired, Stun, Rage, Die };
+
+	float startx;
+	float starty;
+
+	public GameObject downfirePreFab;
+	public GameObject flashfirePreFab;
+
+	//Rigidbody2D rbody;
+	Animator anim;
+	B1State currstate;
+	Transform target; //target Wallow
+
+	int timer;
+	int attackno;
+	int health;
+	int hitblink;
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator> ();
+		anim.SetBool("Hit_Blink", false);
+		target = GameObject.FindWithTag("Player").transform; //target the player
+		startx = transform.position.x;
+		starty = transform.position.y;
+
+		SetRage ();
+		health = 100;
+		hitblink = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Reduce Timer for current action
+		timer--;
+		//Update current action
+		switch (currstate) {
+			//Idle
+		case B1State.Idle:
+			if (timer <= 0) {
+				SetAlign();
+			}
+			break;
+			//Align
+		case B1State.Align:
+			if (timer <= 0) {
+				SetAttack();
+			} else {
+				if (target.transform.position.x < (transform.position.x-0.1)) {
+					transform.position -= transform.right * 4 * Time.deltaTime; 
+				} else if (target.transform.position.x > (transform.position.x+0.1)) {
+					transform.position += transform.right * 4 * Time.deltaTime; 
+				}
+			}
+			break;
+			//Attack
+		case B1State.Attack:
+			if (timer <= 0) {
+				//Create fireball
+				GameObject Clone;
+				Clone = (Instantiate (downfirePreFab, (transform.position - transform.up),
+					transform.rotation)) as GameObject;
+				if (attackno < 3) {
+					SetAlign();
+				} else {
+					SetTired();
+				}
+			}
+			break;
+			//Tired
+		case B1State.Tired:
+			if (timer <= 0) {
+				SetAlign();
+			}
+			break;
+			//Stun
+		case B1State.Stun:
+			if (timer <= 0) {
+				SetRage();
+			}
+			break;
+			//Rage
+		case B1State.Rage:
+			if (timer <= 0) {
+				//Create the side fireballs
+				GameObject Clone;
+				Clone = (Instantiate (flashfirePreFab, (transform.position - transform.up),
+					transform.rotation)) as GameObject;
+				Clone = (Instantiate (flashfirePreFab, (transform.position - transform.right),
+					transform.rotation)) as GameObject;
+				Clone = (Instantiate (flashfirePreFab, (transform.position + transform.right),
+					transform.rotation)) as GameObject;
+				Clone = (Instantiate (flashfirePreFab, (transform.position - transform.up + transform.right),
+					transform.rotation)) as GameObject;
+				Clone = (Instantiate (flashfirePreFab, (transform.position - transform.up - transform.right),
+					transform.rotation)) as GameObject;
+				SetIdle();
+			}
+			break;
+			//Die
+		case B1State.Die:
+			if (timer <= 0){
+				Destroy (this.gameObject);
+			}
+			break;
+		}//End State Update
+		//Update Animator States
+		if (hitblink > 0){
+			anim.SetBool("Hit_Blink", true);
+			hitblink--;
+		}else{
+			anim.SetBool("Hit_Blink", false);
+		}
+		float i = (float)currstate;
+		anim.SetFloat ("State_Flag", i);
+	}
+		
+	void ApplyDamage(int damageamount){
+
+		if (currstate == B1State.Align || currstate == B1State.Attack
+			|| currstate == B1State.Tired) {
+			SetStun ();
+		}
+		if ((hitblink <= 0) && (currstate == B1State.Stun)) {
+			hitblink = 30;
+			anim.SetBool("Hit_Blink", true);
+			health -= damageamount;
+			if (health <= 0) {
+				SetDie ();
+			}
+		}
+	}
+
+	void SetIdle(){
+		timer = 60;
+		currstate = B1State.Idle;
+		attackno = 0;
+	}
+	void SetAlign(){
+		timer = 80;
+		currstate = B1State.Align;
+	}
+	void SetAttack(){
+		timer = 20;
+		currstate = B1State.Attack;
+		attackno++;
+	}
+	void SetTired(){
+		timer = 120;
+		currstate = B1State.Tired;
+		attackno = 0;
+	}
+	void SetStun(){
+		timer = 120;
+		currstate = B1State.Stun;
+		attackno = 0;
+	}
+	void SetRage(){
+		timer = 60;
+		currstate = B1State.Rage;
+		attackno = 0;
+	}
+	void SetDie(){
+		timer = 30;
+		currstate = B1State.Die;
+	}
+}
---
+++ Animation Experiments/Assets/Scripts/BossTwo_Movement.cs
@@ -0,0 +1,229 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BossTwo_Movement : MonoBehaviour {
+
+	enum B2State {Idle, Summon, Chase, Attack, Die};
+
+	public GameObject lionPreFab;
+	public GameObject whipPreFab;
+	public AudioClip hitSound;
+	public AudioClip dieSound;
+	private AudioSource source;
+	private float volLowRange = .5f;
+	private float volHighRange = 1.0f;
+	//public lionsab;
+	float startx;
+	float starty;
+	float summonx;
+	float summony;
+
+	//Rigidbody2D rbody;
+	Animator anim;
+	B2State currstate;
+	Transform target; //target Wallow
+	float dir_x;
+	float dir_y;
+
+	int timer;
+	int attackblink;
+	int health;
+	int hitblink;
+
+	// Use this for initialization
+	void Start () {
+		SetIdle ();
+		health = 10;
+		attackblink = 0;
+		hitblink = 0;
+		source = GetComponent<AudioSource>();
+		anim = GetComponent<Animator> ();
+		target = GameObject.FindWithTag("Player").transform; //target the player
+		startx = transform.position.x;
+		starty = transform.position.y;
+		summonx = (float)startx;
+		summony = (float)(starty + 1.5);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Reduce timer for current action
+		timer--;
+		//Update current action
+		switch(currstate)
+		{
+		//Idle
+		case B2State.Idle:
+			if (timer <= 0) {
+				SetSummon ();
+			}
+			break;
+		//Summon
+		case B2State.Summon:
+			if (timer <= 0) {
+				SetChaseStart ();
+			} else if (timer % 30 == 15) {
+				//Create lion
+				//if(!lionsab){
+				Instantiate (lionPreFab, new Vector2(Random.Range(startx-1.5F, startx+1.5F), starty),
+					transform.rotation);
+				//}
+			}
+			break;
+		//Chase
+		case B2State.Chase:
+			if (timer <= 0) {
+				SetSummon ();
+			} else{
+				//Find distance to player
+				float distx = target.position.x - transform.position.x;
+				float disty = target.position.y - transform.position.y;
+				if (distx <= 0) {
+					dir_x = -1;
+				} else {
+					dir_x = 1;
+				}
+				if (disty <= 0) {
+					dir_y = -1;
+				} else {
+					dir_y = 1;
+				}
+				//Near-perfect vertical alignment
+				if (distx >= -0.65 && distx <= 0.65) {
+					//face player
+					dir_x = 0;
+					//Attack if in range, else move into range
+					if (disty >= -0.95 && disty <= 0.95) {
+						SetAttack ();
+					} else {
+						MoveOn ();
+					}
+				}
+				//Near-perfect horizontal alignment
+				else if (disty >= -0.65 && disty <= 0.65) {
+					//face player
+					dir_y = 0;
+					//Attack if in range, else move into range
+					if (distx >= -0.45 && distx <= 0.45) {
+						SetAttack ();
+					} else {
+						MoveOn ();
+					}
+				}
+				//Not aligned either way
+				else {
+					MoveOn ();
+				}
+			
+			}
+			break;
+		//Attack
+		case B2State.Attack:
+			attackblink--;
+			if (attackblink <= 0) {
+				SetChaseContinue ();
+			} else if (attackblink == 15) {
+				//Summon whip image
+				if (dir_y == -1) {
+					Instantiate (whipPreFab, (transform.position - transform.up),
+						transform.rotation);
+				} else if (dir_y == 1) {
+					Instantiate (whipPreFab, (transform.position + transform.up),
+						transform.rotation);
+				} else if (dir_x == -1) {
+					Instantiate (whipPreFab, (transform.position - transform.right),
+						transform.rotation);
+				} else {
+					Instantiate (whipPreFab, (transform.position + transform.up),
+						transform.rotation);
+				}
+
+			}
+			break;
+		//Die
+		case B2State.Die:
+			if (timer <= 0) {
+				Destroy (this);
+				Destroy (this.gameObject);
+			}
+			break;
+		}//End update current action
+		//Update Animator States
+		if (hitblink > 0){
+			hitblink--;
+		}else{
+			anim.SetBool("Hit_Blink", false);
+		}
+		float i = (float)currstate;
+		anim.SetFloat("State_Flag", i);
+		anim.SetFloat ("X_Dir", dir_x);
+		anim.SetFloat ("Y_Dir", dir_y);
+
+	}
+
+	//Take Damage
+	void ApplyDamage(int damageamount)
+	{
+		if (hitblink <= 0) {
+			float vol = Random.Range (volLowRange, volHighRange);
+			source.PlayOneShot (hitSound, vol);
+			hitblink = 30;
+			health -= damageamount;
+			if (health <= 0) {
+				SetDie ();
+			}
+		}
+	}
+
+	void SetIdle(){
+		timer = 40;
+		currstate = B2State.Idle;
+		dir_x = 0;
+		dir_y = 0;
+	}
+	void SetSummon(){
+		timer = 300;
+		currstate = B2State.Summon;
+		dir_x = 0;
+		dir_y = 0;
+		transform.position = new Vector2(summonx, summony);
+	}
+	void SetChaseStart(){
+		timer = 300;
+		currstate = B2State.Chase;
+		transform.position = new Vector2(startx, starty);
+		dir_x = 0;
+		dir_y = 0;
+	}
+	void SetChaseContinue(){
+		currstate = B2State.Chase;
+	}
+	void SetAttack(){
+		currstate = B2State.Attack;
+		attackblink = 60;
+	}
+	void SetDie(){
+		timer = 60;
+		Destroy (this);
+		Destroy (this.gameObject);
+		float vol = Random.Range (volLowRange, volHighRange);
+		source.PlayOneShot(dieSound,vol);
+		currstate = B2State.Die;
+	}
+
+	void MoveOn(){
+		//Move in current direction
+		if(dir_x < 0){
+			transform.position -= transform.right * 3 * Time.deltaTime;
+		}else if(dir_x > 0){
+			transform.position += transform.right * 3 * Time.deltaTime;
+		}
+		if(dir_y < 0){
+			transform.position -= transform.up * 2 * Time.deltaTime;
+		}else if(dir_y > 0){
+			transform.position += transform.up * 2 * Time.deltaTime;
+		}
+
+	}
+
+}
