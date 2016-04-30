763e8d46915fc7d5649e611fb290a9f0d53e0642
Tecku <sethbreloom94@gmail.com>
2016-04-30 14:15:22 -0500

    My Stuff
    Do not merge this stuff, just use it as a template to get your new
    stuff.

---
+++ All 'Bout Wallow/Assets/Scripts/scene interaction scripts/I_1Intro.cs
@@ -0,0 +1,23 @@
+﻿// http://docs.unity3d.com/410/Documentation/ScriptReference/index.Accessing_Other_Game_Objects.html
+// https://unity3d.com/learn/tutorials/projects/2d-roguelike-tutorial/writing-game-manager
+using UnityEngine;
+using System.Collections;
+
+public class I_1Intro : MonoBehaviour {
+
+	GameObject manager;
+
+	// Use this for initialization
+	void Start () {
+		manager = GameObject.Find("gameManager");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// // if player goes outside wallow's door go to 1.2
+		if (){
+			manager.GameManager.ChangeToScene("1.2 Cake and Insults");
+		}
+		// else do nothing
+	}
+}
